dbe555f3c3b7b191cec23013d6035a02e0d73e05
uonoro <uonoro@email.de>
2025-02-28 18:30:45 +0100

    sdf

--- electron/main/FileManager.ts
+++ electron/main/FileManager.ts
@@ -1,4 +1,7 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
 const { dialog } = require("electron");
+
 const fs = require("fs");
 
 export const selectFile = () => {
